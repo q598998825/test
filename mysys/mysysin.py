a4f0e1a249726d35941f55e6b00e2a3653e93520
q598998825 <598998825@qq.com>
2019-06-04 17:48:04 +0800

    Update mysysin.py

--- mysys/mysysin.py
+++ mysys/mysysin.py
@@ -2,8 +2,20 @@
 from mysocket import *
 from mymsg import *
 
+def Serverfunc(serv,sock,data):
+    print(data)
+    serv.resoponse(sock,"12345654987")
+
 def Init():
     mysocket1 = mysocket()
+    socket1 = mysocket1.sockInit(port=9090,IsClient =False)
+    if (socket1 is None):
+        print("分配失败")
+    else:
+        print("链接成功")
+        mysocketServerPool1 = mysocketServerPool()
+        mysocketServerPool1.addSocket(socket1,Serverfunc)
+        mysocketServerPool1.run()
     pass
 
 def Proc(msg):
