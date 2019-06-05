e0d7d15fabb92ef38320fa3b14f9b19fa0ea894e
q598998825 <598998825@qq.com>
2019-06-05 11:25:11 +0800

    Update mymain.py

--- mymain.py
+++ mymain.py
@@ -1,8 +1,14 @@
 # coding=utf-8
 from mysys.mysys import *
+import time, logging
 
+def logInit():
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
+    return 0
 
 if __name__ == '__main__':
+    logInit()
     mysys1 = mysys()
     mysys1.InitEnv()
     while True:
