b5a29c66b05ddeec2878c49703797106545d8e4d
q598998825 <598998825@qq.com>
2019-06-05 10:10:34 +0800

    Update mysysin.py

--- mysys/mysysin.py
+++ mysys/mysysin.py
@@ -1,18 +1,33 @@
 # coding=utf-8
 from mysocket import *
 from mymsg import *
+import logging
 
 def Serverfunc(serv,sock,data):
-    print(data)
-    serv.resoponse(sock,"12345654987")
+    logging.debug(data)
+    serv.resoponse(sock,"12345654987".encode())
+
+def logInit():
+    logging.basicConfig(level=logging.DEBUG)
+    return -1
+
+
+
+g_MySysInInitFunc = [logInit]
 
 def Init():
+    global g_MySysInInitFunc
+    for func in g_MySysInInitFunc:
+        if(0 > func()):
+            logging.error("%s failed!!!"%str(func))
+            exit(1)
+
     mysocket1 = mysocket()
     socket1 = mysocket1.sockInit(port=9090,IsClient =False)
     if (socket1 is None):
-        print("分配失败")
+        logging.error("分配失败")
     else:
-        print("链接成功")
+        logging.debug("链接成功")
         mysocketServerPool1 = mysocketServerPool()
         mysocketServerPool1.addSocket(socket1,Serverfunc)
         mysocketServerPool1.run()
