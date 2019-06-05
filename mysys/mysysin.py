7a2ca2f9183727e2d6b51aabf04e8a41b1176468
q598998825 <598998825@qq.com>
2019-06-05 11:26:10 +0800

    Update mysysin.py

--- mysys/mysysin.py
+++ mysys/mysysin.py
@@ -1,26 +1,24 @@
 # coding=utf-8
 from mysocket import *
 from mymsg import *
-import logging
+import logging, os
 
 def Serverfunc(serv,sock,data):
     logging.debug(data)
     serv.resoponse(sock,"12345654987".encode())
 
-def logInit():
-    logging.basicConfig(level=logging.DEBUG)
-    return -1
-
-
-
-g_MySysInInitFunc = [logInit]
+g_MySysInInitFunc = ["myMsgInit"]
 
 def Init():
     global g_MySysInInitFunc
     for func in g_MySysInInitFunc:
-        if(0 > func()):
-            logging.error("%s failed!!!"%str(func))
-            exit(1)
+        try:
+            if(0 > eval(func)()):
+                logging.error("%s failed!!!"%func)
+                os._exit(1)
+        except Exception as e:
+            logging.error("基础函数[%s]初始化异常：%s" % (func, e.__str__()))
+            os._exit(1)
 
     mysocket1 = mysocket()
     socket1 = mysocket1.sockInit(port=9090,IsClient =False)
@@ -35,3 +33,7 @@ def Init():
 
 def Proc(msg):
     pass
+
+def myMsgInit():
+    mymsg1 = mymsg(123,456)
+    return 0
