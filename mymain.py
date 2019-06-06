f7c63416c907f7c74b8a6be1fb72fdafb887f201
q598998825 <598998825@qq.com>
2019-06-06 14:59:33 +0800

    Update mymain.py

--- mymain.py
+++ mymain.py
@@ -3,6 +3,7 @@ from mysys.mysys import *
 import time, logging
 
 def logInit():
+    #因为此函数之前不能有logging的调用故放此处
     logging.basicConfig(level=logging.DEBUG,
                         format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s')
     return 0
