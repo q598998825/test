aede9eb4272449bc4279d1a96b7e7cd1383c07c9
q598998825 <598998825@qq.com>
2019-06-30 22:28:57 +0800

    提交代码

--- src/vprogram/test2.py
+++ src/vprogram/test2.py
@@ -2,6 +2,7 @@
 from mycomm import *
 from mymsg import *
 import logging
+from mytimer import *
 
 def Init():
     logging.debug("test2 Init")
@@ -10,6 +11,8 @@ def Init():
     MyMsgPkg1.Data = "123213123"
     sendMsgself(MyMsgPkg1)
     sendMsgself(MyMsgPkg1)
+    myTimer1 = myTimer('from1', 'test1', 'msg1', 1)
+    myTimer2 = myTimer('from2', 'test1', 'msg2', 2)
     return 0
 
 
