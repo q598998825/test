3a0bb09879d58509ad04bee182f6a8c2cbaf0198
q598998825 <598998825@qq.com>
2020-08-27 16:32:45 +0800

    Create vscript_func.py

---
+++ src/vprogram/vscript/vscript_func.py
@@ -0,0 +1,15 @@
+import logging
+from mymsg import *
+from vscript import *
+from vscript_dm import *
+
+def Init():
+    logging.debug("vscript_func Init")
+    vscript_list()
+    vscript1=vscript()
+    vscript1.run()
+    logging.debug("vscript_func Init end")
+    return 0
+
+def Proc(msg:MyMsgPkg):
+    logging.debug("vscript_func Proc %s"%(msg.Data))
