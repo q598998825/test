fd9a8c47e694831b415fca4154a3a3b3ab96e1ec
q598998825 <598998825@qq.com>
2020-08-27 16:32:16 +0800

    Create vscript_dm.py

---
+++ src/vprogram/vscript/vscript_dm.py
@@ -0,0 +1,24 @@
+import logging,os
+from vscript_rule import *
+from mydatabase import *
+
+class vscript_list(mydatabase_table):
+
+    table_infos = {"vscript_list": {"table_name": "vscript_list",
+                                 "desc": "脚本列表",
+                                 "table_col": [{"col": "NAME", "opt": " VARCHAR(64) PRIMARY KEY NOT NULL"},
+                                               {"col": "FILENAME", "opt": " VARCHAR(128) NOT NULL"},  # 类型，事物的类型
+                                               {"col": "STATE", "opt": "NUMBER "},  # 1为正常数据，0为无没数据
+                                               {"col": "DESC", "opt": " VARCHAR(128)"}  # 注释
+                                               ]}
+                   }
+
+    defaultSql = {"getList":"select NAME,FILENAME,STATE,DESC from vscript_list where state = 1"}  # 常用语句制作
+
+    def __init__(self):
+        super().__init__()
+        self.Init()
+
+    def Init(self):
+        super().Init()
+        #self.DefaultData()
