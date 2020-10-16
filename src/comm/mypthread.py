ab5543234f9b7063e385dc8dc15e0fb862bf7236
q598998825 <598998825@qq.com>
2020-10-16 11:40:54 +0800

    Update mypthread.py

--- src/comm/mypthread.py
+++ src/comm/mypthread.py
@@ -1,16 +1,26 @@
 # coding=utf-8
-import threading
+import threading,logging
+from contextlib import contextmanager
+from globalConfig import *
 
-# 创建全局ThreadLocal对象:
-lThreadPool = threading.local()
+@contextmanager
+def call():
+    #logging.debug("opcode:%s,lThreadPool:%s begin" % (str(lThreadPool.opcode), str(lThreadPool)))
+    yield
+    #logging.debug("opcode:%s,lThreadPool:%s end" % (str(lThreadPool.opcode), str(lThreadPool)))
+
+
 
 class mypthread(threading.Thread):
-    def __init__(self, func, arg):
+    def __init__(self, func, arg,opcode):
         super(mypthread, self).__init__()  # 注意：一定要显式的调用父类的初始化函数。
         self.arg = arg
         self.func = func
+        self.opcode = opcode
 
     def run(self):  # 定义每个线程要运行的函数
-        self.func(self.arg)
+        lThreadPool.opcode = self.opcode
+        with call():
+            self.func(self.arg)
 
 
