bc8c2bc9b0646b5e817d032aadd1eb52cdf8c52a
q598998825 <598998825@qq.com>
2020-10-30 19:18:07 +0800

    Update install.py

--- install.py
+++ install.py
@@ -1,18 +1,17 @@
 # coding=utf-8
 import os
-
+from wsgiref.simple_server import make_server
+'''
+循环安装脚本
+'''
 install_array = {"jieba":["pip install jieba"]}
 
-str = input("请输入安装类型：");
 '''
 test123
 '''
-if str != "all":
-    if str not in install_array:
-        print("无此类型的安装命令")
-    for cmd in  install_array[str]:
-        os.system(cmd)
-else:
-    for install_type in install_array:
+for install_type in install_array:
+    try:
+        eval("import %s"%install_type)
+    except:
         for cmd in install_array[install_type]:
             os.system(cmd)
