eefd28c4da8ef9a14364913d52dbc6165f5b3549
q598998825 <598998825@qq.com>
2020-11-06 16:31:10 +0800

    Update vscript_dm.py

--- src/vprogram/vscript/vscript_dm.py
+++ src/vprogram/vscript/vscript_dm.py
@@ -11,10 +11,19 @@ class vscript_list(mydatabase_table):
                                                {"col": "FILENAME", "opt": " VARCHAR(128) NOT NULL"},  # 类型，事物的类型
                                                {"col": "STATE", "opt": "NUMBER "},  # 1为正常数据，0为无没数据
                                                {"col": "DESC", "opt": " VARCHAR(128)"}  # 注释
-                                               ]}
+                                               ]},
+                   "tmpData": {"table_name": "tmpData",
+                                    "desc": "脚本临时数据列表",
+                                    "table_col": [{"col": "KEY", "opt": " VARCHAR(128) PRIMARY KEY NOT NULL"},
+                                                  {"col": "VALUE", "opt": " VARCHAR(1024)"},  # 值
+                                                  {"col": "updateDate", "opt": "DATE "},  # 更新时间
+                                                  {"col": "DESC", "opt": " VARCHAR(128)"}  # 注释
+                                                  ]}
                    }
 
-    defaultSql = {"getList":"select NAME,FILENAME,STATE,DESC from vscript_list where state = 1"}  # 常用语句制作
+    defaultSql = {"getList":"select NAME,FILENAME,STATE,DESC from vscript_list where state = 1",
+                  "getData":"select KEY,VALUE,updateDate,DESC from tmpData where KEY = '%s'",
+                  "setData":"insert or replace into tmpData (KEY,VALUE,updateDate) VALUES ('%s',(%s),datetime())"}  # 常用语句制作
 
     def __init__(self):
         super().__init__()
